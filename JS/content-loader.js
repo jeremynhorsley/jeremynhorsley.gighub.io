4c41a19633a467ba5ab43d1360b4331d036ddba5
Jeremyn Horsley <jeremynhorsley@gmail.com>
2019-05-11 12:56:23 -0700

    cleanup

---
+++ JS/content-loader.js
@@ -0,0 +1,45 @@
+
+
+function loadHome() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("content").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "HTML/home.html", true);
+  xhttp.send();
+};
+
+function loadAbout() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("content").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "HTML/about.html", true);
+  xhttp.send();
+};
+
+function loadProjects() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("content").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "HTML/projects.html", true);
+  xhttp.send();
+};
+
+function loadContact() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("content").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "HTML/contact.html", true);
+  xhttp.send();
+};
